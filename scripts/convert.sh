67c3e2a499215d86a2a50d2417bae3c4fe7e2196
Jonathan Schäfer <jonathan.schaefer@mailbox.org>
2020-12-18 14:28:52 +0100

    feat: begin conversion implementation Refs:#2 #3

---
+++ scripts/convert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+filetype=$1
+source_path=$2
+csv_path=$3
+chromsizes=$4
+
+new_link=$source_path/linking.csv
+
+
+validate_file () {
+	true
+}
+
+validate_required() {
+	true
+}
+
+convert_files() {
+	true
+}
+
+validate_required
+
+while IFS="," read -r genome source seq_type tf data_type file_type file_path remaining
+do
+	if test -e "source_path/$file_path"; then
+		continue
+	fi
+	if [ "$file_type" != "$filetype" ]; then
+		source_file="$source_path$file_path"
+		validate_filetype "$source_file" "$seq_type"
+		convert_file "$source_file" "$filetype" "$chromsizes/$genome"
+	fi
+	echo "$genome,$source,$seq_type,$tf,$data_type,$file_type,$FILE,$remaining" >> "$new_link"
+done < <(tail -n +2 "$csv_path")
