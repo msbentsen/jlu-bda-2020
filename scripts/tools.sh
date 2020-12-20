e54d0ac929c8ad4ca8d37d4227723bbd4106c274
Jonathan Schäfer <jonathan.schaefer@mailbox.org>
2020-12-20 17:00:17 +0100

    docs: add documentation to tools.sh

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+#===============================================================================
+#
+#  FILE:  tools.sh
+#
+#  USAGE:  tools.sh [tools directory] [list of tools]
+#
+#  DESCRIPTION:  Check for the availability of ucsc tools. Additional arguments
+#  my be given to check for additional tools. If a tool is not found the script
+#  attempts to install it.
+#
+#  NOTES:  ---
+#  AUTHOR:  Jonathan Schäfer
+#===============================================================================
+
 tools_path="$1/tools/"
 mkdir --parents "$tools_path"
 min_tools=("bedGraphToBigWig" "bigWigToBedGraph" "bigWigMerge")
 
+#==== Function =================================================================
+#  Name: validate
+#  Description: attempt to exectute a tool to see if it is callable
+#  Parameter 1: name of the tool to validate
+#===============================================================================
 validate() {
 	"$tools_path$1"  >/dev/null 2>&1
 	if [ "$?" == "127" ]; then
@@ -12,12 +31,22 @@ validate() {
     return 0
 }
 
+#==== Function =================================================================
+#  Name: install_tool
+#  Description: attempt to install a tool into the tool directory
+#  Parameter 1: name of the tool to validate
+#===============================================================================
 install_tool() {
    wget --quiet --tries 2 "https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/$1"
    chmod u+x "./$1"
    mv "./$1" "$tools_path"
 }
 
+#==== Function =================================================================
+#  Name: check
+#  Description: checks wheter a tool is available and attempts to install if not
+#  Parameter 1: name of the tool to validate
+#===============================================================================
 check () {
     validate "$1"
     local result=$?
@@ -32,6 +61,10 @@ check () {
     fi
 }
 
+#===============================================================================
+#  goes through an array of minimally required tools to check/install and then
+#  continues to go through every argument to check if necessary
+#===============================================================================
 for tool in "${min_tools[@]}"
 do
     check "$tool"
