710570227c58c3d1460b0ec473cf409c9d27559d
Jonathan Schäfer <jonathan.schaefer@mailbox.org>
2021-01-15 10:03:02 +0100

    feat: sort files & update csv Refs: #4

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#===============================================================================
+#
+#  FILE:  sort.sh
+#
+#  USAGE:  sort.sh [data directory path] [destination directory path] [csv_path]
+#
+#  DESCRIPTION:  Sort files in data into a folderstructure in the destination
+#				according to the parameters in the csv table
+#
+#  NOTES:  ---
+#  AUTHOR:  Jonathan Schäfer
+#===============================================================================
 
 source_path=$1
 dest_path=$2
@@ -7,22 +19,30 @@ csv_path=$3
 new_link=$dest_path/linking.csv
 head -n1 "$csv_path" > "$new_link"
 
-while IFS="," read -r genome source seq_type tf data_type file_type file_path remaining
+
+#===============================================================================
+# Goes through all lines of the .csv and sorts the files according to the
+# sequencing technique.
+#===============================================================================
+
+while IFS="," read -r genome source seq_type tf data_type file_type \
+file_path remaining
 do
-	if test -e "source_path/$file_path"; then
+	if [ ! -e "$source_path/$file_path" ]; then
 		continue
 	fi
 
-	if [ "$seq_type" == "atac_seq" ]; then
+	if [ "$seq_type" == "ATAC-Seq" ]; then
 		new_path=$dest_path/$source/$seq_type/
-	elif [ "$seq_type" == "chip_seq" ]; then
+	elif [ "$seq_type" == "ChIP-seq" ]; then
 		new_path=$dest_path/$source/$seq_type/$tf/
-	elif [ "$seq_type" == "dns_seq" ]; then
+	elif [ "$seq_type" == "DNase-seq" ]; then
 		new_path=$dest_path/$source/$seq_type/$tf/
 	else
-		echo "error, invalid sequencing technique"
+		echo "error, invalid sequencing technique for file $file_path"
 		exit 1
 	fi
+
 	filename=$(basename file_path)
 	mkdir -p "$new_path"
 	sourcefile="$source_path$file_path"
