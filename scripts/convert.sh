db24ece789d283e0c059fa3e50ad0e6735f00cba
Jonathan Schäfer <jonathan.schaefer@mailbox.org>
2021-01-17 15:53:13 +0100

    feat: implement validation and update for new .csv Refs: #2 #3

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,10 +6,33 @@ csv_path=$3
 chromsizes=$4
 
 new_link=$source_path/linking.csv
-
+export new_filename=""
 
 validate_file () {
-	true
+	local file_extension=${$1##*.}
+	local filetype
+	if [ $file_extension != "$2" ]; then
+		case "$3" in
+		"CHROMOSOME,START,END,VALUE")
+			filetype="bedgraph"
+			;;
+		"CHROMOSOME,START,END,NAME,SCORE,STRAND,SIGNAL_VALUE,P_VALUE,Q_VALUE,PEAK" \
+		| "CHROMOSOME,START,END,NAME,SCORE,STRAND,SIGNAL_VALUE,P_VALUE,Q_VALUE" \
+		| "CHROMOSOME,START,END,NAME,SCORE"\
+		| "CHROMOSOME,START,END,NAME")
+			filetype="bed"
+			;;
+		*)
+			echo "unrecognized file format"
+			filetype=""
+			;;
+		esac
+		if [ "$filetype" != "$2" ]; then
+					new_filename="$(basename "$1").$filetype"
+			return 1
+		fi
+	fi
+	return 0
 }
 
 
@@ -17,15 +40,24 @@ convert_files() {
 	true
 }
 
-while IFS="," read -r genome source seq_type tf data_type file_type file_path remaining
+while IFS="," read -r experiment_id	genome	biosource	technique	\
+	epigenetic_mark	filename	data_type	extension	format	remaining
 do
-	if test -e "source_path/$file_path"; then
+	if [ ! -e "source_path/$filename" ]; then
 		continue
 	fi
-	if [ "$file_type" != "$filetype" ]; then
-		source_file="$source_path$file_path"
-		validate_filetype "$source_file" "$seq_type"
+
+	if [ "$extension" != "$filetype" ]; then
+		source_file="$source_path$filename"
+		if [[ "$(validate_filetype "$filename" "$extension" "$format")" != "0" ]]; then
+			filename=${new_filename%.*}
+			extension=${new_filename##*.}
+		fi
 		convert_file "$source_file" "$filetype" "$chromsizes/$genome"
 	fi
-	echo "$genome,$source,$seq_type,$tf,$data_type,$file_type,$FILE,$remaining" >> "$new_link"
+	echo "$experiment_id,$genome,$biosource,$technique	\
+	,$epigenetic_mark,$filename,$data_type,$extension,$format,$remaining"\
+	>> "$new_link"
 done < <(tail -n +2 "$csv_path")
+
+mv "$new_link" "$source_path/linking_table.csv"
