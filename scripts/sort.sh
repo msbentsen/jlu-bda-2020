1139f171d4fa6c5c031e373dabe615080e176485
Jonathan Schäfer <jonathan.schaefer@mailbox.org>
2021-01-17 15:41:27 +0100

    feat: update to new .csv and insert filepath Closes: #4

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -6,7 +6,8 @@
 #  USAGE:  sort.sh [data directory path] [destination directory path] [csv_path]
 #
 #  DESCRIPTION:  Sort files in data into a folderstructure in the destination
-#				according to the parameters in the csv table
+#				according to the parameters in the csv table, At the same time
+#				adds the new filepath and cleans up the .csv
 #
 #  NOTES:  ---
 #  AUTHOR:  Jonathan Schäfer
@@ -16,37 +17,39 @@ source_path=$1
 dest_path=$2
 csv_path=$3
 
-new_link=$dest_path/linking.csv
-head -n1 "$csv_path" > "$new_link"
+new_link=$dest_path/linking_table.csv
+
+headers=$(head -n1 "$csv_path")
+echo "${headers:0:77}filepath,${headers:77}" > "$new_link"
 
 
 #===============================================================================
 # Goes through all lines of the .csv and sorts the files according to the
-# sequencing technique.
+# sequencing technique, skipping non existing files to clean up the .csv .
 #===============================================================================
 
-while IFS="," read -r genome source seq_type tf data_type file_type \
-file_path remaining
+while IFS="," read -r experiment_id	genome	biosource	technique	\
+	epigenetic_mark	filename	data_type	extension	remaining
 do
-	if [ ! -e "$source_path/$file_path" ]; then
+	if [ ! -e "$source_path/$filename" ]; then
 		continue
 	fi
 
-	if [ "$seq_type" == "ATAC-Seq" ]; then
-		new_path=$dest_path/$source/$seq_type/
-	elif [ "$seq_type" == "ChIP-seq" ]; then
-		new_path=$dest_path/$source/$seq_type/$tf/
-	elif [ "$seq_type" == "DNase-seq" ]; then
-		new_path=$dest_path/$source/$seq_type/$tf/
+	if [ "$technique" == "ATAC-Seq" ]; then
+		new_path=$dest_path/$biosource/$technique/
+	elif [ "$technique" == "ChIP-seq" ]; then
+		new_path=$dest_path/$biosource/$technique/$epigenetic_mark/
+	elif [ "$technique" == "DNase-seq" ]; then
+		new_path=$dest_path/$biosource/$technique/
 	else
-		echo "error, invalid sequencing technique for file $file_path"
+		echo "error, invalid sequencing technique for file $filename"
 		exit 1
 	fi
 
-	filename=$(basename file_path)
 	mkdir -p "$new_path"
-	sourcefile="$source_path$file_path"
+	sourcefile="$source_path$filename"
 	newfile="$new_path$filename"
 	mv "$sourcefile" "$newfile"
-	echo "$genome,$source,$seq_type,$tf,$data_type,$file_type,$FILE,$remaining" >> "$new_link"
+	echo "$experiment_id,$genome,$biosource,$technique,$epigenetic_mark,\
+	$filename,$data_type,$newfile,$extension,$remaining" >> "$new_link"
 done < <(tail -n +2 "$csv_path")
