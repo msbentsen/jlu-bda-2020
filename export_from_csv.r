5a08bad7cfcf2ce3d972c520a962b44335f3b1be
Patrick <decrepitasylums@gmail.com>
2021-01-19 22:39:42 +0100

    push for testing

--- export_from_csv.r
+++ export_from_csv.r
@@ -1,9 +1,10 @@
 exportFromCsv <- function(file) {
 
-  data <- read.csv(file=file,header=TRUE)
-  chrs <- c(1:22,"X","Y")
+  # How this works: For each experiment and each chromosome, download all regions as a tab-separated text file.
+  # The hg19 and Grch38 genomes contain more than 24 chromosomes, but we only use the common ones.
   
-  # for each experiment and each chromosome of that experiment, download
+  data <- read.csv(file=file,header=TRUE)
+  chrs <- paste("chr",c(1:22,"X","Y"),sep="")
   
   for(row in 1:nrow(data)) {
     e = data[row,]
@@ -12,10 +13,13 @@ exportFromCsv <- function(file) {
     format = e$format
     for(c in chrs) {
       query_id = deepblue_select_experiments(experiment_name = name, chromosome = c)
-      request_id = deepblue_get_regions(query_id = query_id, output_format = format) # output_format required -> problem
-      regions = deepblue_download_request_data(request_id)
-      deepblue_export_tab(regions,file.name=name)
-      deepblue_export_meta_data(id, file.name=paste(name,".",c))
+      request_id = deepblue_get_regions(query_id = query_id, output_format = format) # output_format required
+      regions = try(deepblue_download_request_data(request_id),silent=TRUE) # ignore errors when no regions found for this chromosome (this is not unusual)
+      if(class(regions)=="GRanges") {
+        filename = paste(name,".",c,sep="") 
+        deepblue_export_tab(regions,file.name=filename)
+        deepblue_export_meta_data(id, file.name=filename)
+      }
     }
   }
 }
