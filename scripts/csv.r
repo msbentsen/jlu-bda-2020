266b74a032cf583e1003b4bf66b4d97c9e73d3a0
Patrick <decrepitasylums@gmail.com>
2021-02-11 16:17:14 +0100

    feat: more arguments and feedback
    Previously, the script would link data regardless of genome, which has been fixed. It also returns more error messages and checks whether all arguments are in accordance with the Deepblue db.

--- scripts/csv.r
+++ scripts/csv.r
@@ -3,28 +3,36 @@ required_packages <- c("BiocManager","data.table","argparse")
 install.packages(setdiff(required_packages,rownames(installed.packages())),repos="http://cran.us.r-project.org")
 if(!"DeepBlueR" %in% installed.packages()) BiocManager::install("DeepBlueR")
 library(argparse)
-library(data.table)
-suppressPackageStartupMessages(library(DeepBlueR))
+
+chrs <- paste("chr",c(1:22,"X","Y"),sep="")
 
 parser <- ArgumentParser()
 
 parser$add_argument("-g", "--genomes", nargs="+", type="character", default=c("hg19","GRCh38"),
-                    choices= suppressMessages(deepblue_list_genomes()$name),
                     help="(List of) genomes to include [default: \"%(default)s\"]")
+parser$add_argument("-c", "--chromosomes", nargs="+", type="character", default=chrs, choices=chrs,
+                    help="(List of) chromosomes to include (requires chr prefix) [default: all]")
+parser$add_argument("-b", "--biosources", nargs="+", type="character", default=NULL,
+                    help="(List of) biosources to include [default: all] (Refer to: https://deepblue.mpi-inf.mpg.de/)")
 parser$add_argument("-t", "--type", nargs="+", type="character", default="peaks", choices=c("peaks","signal"),
                     help="Experiment file types allowed for CHiP-Seq data [default: \"%(default)s\"]")
+parser$add_argument("-a", "--atactype", nargs="+", type="character", default=c("peaks","signal"), choices=c("peaks","signal"),
+                    help="Experiment file types allowed for ATAC/DNAse-Seq data [default: \"%(default)s\"]")
 parser$add_argument("-m", "--marks", nargs="+", type="character", default=NULL,
-                    help="(List of) epigenetic marks (i.e. transcription factors) to include [default: all]")
+                    help="(List of) epigenetic marks (i.e. transcription factors) to include [default: all] (Refer to: https://deepblue.mpi-inf.mpg.de/)")
 parser$add_argument("-o", "--output", type="character", default="linking_table",
                     help="Output file name without extension [default: \"%(default)s\"]")
 
 args <- parser$parse_args()
 
+library(data.table)
+library(DeepBlueR)
+
 # create_linking_table()
 # Takes optional arguments (see above)
 # Creates a csv file built from specific Deepblue data
 
-create_linking_table <- function(genomes,chip_type,chip_marks,outfile) {
+create_linking_table <- function(genomes,chrs,filter_biosources,chip_type,atac_type,chip_marks,outfile) {
   
   # chrom_in_filename()
   # Expects a vector of filenames (strings) and a vector of chromosomes (strings)
@@ -54,6 +62,7 @@ create_linking_table <- function(genomes,chip_type,chip_marks,outfile) {
   }
   
   new_row <- function(metadata,filename) {
+    print(filename)
     e <- metadata$`_id`
     # This function retrieves the metadata for a DeepBlue experiment and converts it into a data.table object
     sample_info <- metadata$sample_info
@@ -76,65 +85,109 @@ create_linking_table <- function(genomes,chip_type,chip_marks,outfile) {
     return(meta)
   }
   
+  all_genomes <- deepblue_list_genomes()$name
+  genomes <- genomes[genomes %in% all_genomes]
+  if(length(genomes) == 0) {
+    stop(paste("No valid genomes provided by user. Available genomes:",paste(all_genomes,collapse=", ")))
+  }
+  
+  if(!is.null(filter_biosources)) {
+    # check whether biosources given by the user are valid
+    all_biosources <- deepblue_list_biosources()$name
+    removed_biosources <- filter_biosources[!filter_biosources %in% all_biosources]
+    n_removed <- length(removed_biosources)
+    if(n_removed>0) warning(paste("dropped",n_removed,"biosource(s):",paste(removed_biosources,collapse=", ")))
+    filter_biosources <- filter_biosources[!filter_biosources %in% removed_biosources]
+    if(length(filter_biosources)==0) {
+      stop("No biosources given by user could be found in Deepblue database")
+    }
+    rm(all_biosources)
+  }
+
+  tf_marks <- deepblue_list_epigenetic_marks(extra_metadata = list(category="Transcription Factor Binding Sites"))$name
+  
+  if(!is.null(chip_marks)) {
+    removed_marks <- chip_marks[!chip_marks %in% tf_marks]
+    n_removed <- length(removed_marks)
+    if(n_removed>0) warning(paste("dropped",n_removed,"tf(s):",paste(removed_marks,collapse=", ")))
+    chip_marks <- chip_marks[!chip_marks %in% removed_marks]
+    if(length(chip_marks)==0) {
+      stop("No transcription factors given by user could be found in Deepblue database")
+    }
+  } else {
+    chip_marks <- tf_marks
+  }
+  
   # ATAC-Seq: signals (mark: DNA Accessibility)
   # DNAse-Seq: signals and peaks (marks: DNA Accessibility, DNAseI)
   
   # 1st Step: Collect biosources for ATAC
   
-  chrs <- paste("chr",c(1:22,"X","Y"),sep="")
-
-  atac <- deepblue_list_experiments(genome=genomes, technique="ATAC-Seq", type="signal", epigenetic_mark="DNA Accessibility")$id
-  dnase <- deepblue_list_experiments(genome=genomes, technique="DNAse-Seq", epigenetic_mark=c("DNA Accessibility","DNaseI"))$id
-  access_experiments <- c(atac,dnase)
+  csv_data = NULL
   
-  atac_metadata <- lapply(access_experiments,function(x){ deepblue_info(x) })
-  atac_biosources <- unique(sapply(atac_metadata,function(x) { x$sample_info$biosource_name }))
-  
-  # 2nd Step: Collect ChiP experiments and add to csv list if biosource has available ATAC data
-  
-  if(is.null(chip_marks)) {
-    chip_marks <- deepblue_list_epigenetic_marks(extra_metadata = list(category="Transcription Factor Binding Sites"))$name
-  }
-  chips <- deepblue_list_experiments(genome=genomes, technique="ChiP-Seq", type=chip_type, epigenetic_mark=chip_marks)$id
-
-  chip_list <- lapply(chips,function(c) {
-    metadata <- deepblue_info(c)
-    biosource <- metadata$sample_info$biosource_name
-    if(biosource %in% atac_biosources) {
-      return_list <- vector("list",length(chrs))
-      filenames <- chrom_in_filename(metadata$name,chrs)
-      for(f in 1:length(filenames)) {
-        return_list[[f]] = new_row(metadata,filenames[f])
-      }
-      return(return_list)
+  for(genome in genomes) {
+    atac <- deepblue_list_experiments(genome=genome, technique="ATAC-Seq", biosource=filter_biosources, type=atac_type)
+    dnase <- deepblue_list_experiments(genome=genome, technique="DNAse-Seq", biosource=filter_biosources, type=atac_type)
+    # Deepblue returns "\n" if no data is matched, otherwise data.frames
+    if(class(atac)[1] != "character") atac = atac$id else atac = NULL
+    if(class(dnase)[1] != "character") dnase = dnase$id else dnase = NULL
+    
+    access_experiments <- c(atac,dnase)
+    
+    if(is.null(access_experiments)) {
+      warning(paste(genome,"No ATAC-seq data available for given arguments",sep=": "))
     } else {
-      return(NA)
-    }
-  })
-  chip_list <- unlist(chip_list[!is.na(chip_list)],recursive = FALSE)
-  
-  chip_biosources <- unique(sapply(chip_list.old,function(x){ x$biosource }))
-  req_biosources <- intersect(atac_biosources,chip_biosources)
-  
-  # 3rd Step: Add ATAC experiments to csv if biosource has available ChiP data
-  
-  atac_list <- lapply(atac_metadata,function(m) {
-    if(m$sample_info$biosource_name %in% req_biosources) {
-      return_list <- vector("list",length(chrs))
-      filenames <- chrom_in_filename(m$name,chrs)
-      for(f in 1:length(filenames)) {
-        return_list[[f]] = new_row(m,filenames[f])
+      atac_metadata <- lapply(access_experiments,function(x){ deepblue_info(x) })
+      atac_biosources <- unique(sapply(atac_metadata,function(x) { x$sample_info$biosource_name }))
+      
+      # 2nd Step: Collect ChiP experiments and add to csv list if biosource has available ATAC data
+      
+      chips <- deepblue_list_experiments(genome=genome, technique="ChiP-Seq", biosource=atac_biosources, type=chip_type, epigenetic_mark=chip_marks)
+      
+      if(class(chips)[1] != "character") {
+        
+        chips = chips$id  
+        chip_list <- lapply(chips,function(c) {
+          metadata <- deepblue_info(c)
+          return_list <- vector("list",length(chrs))
+          filenames <- chrom_in_filename(metadata$name,chrs)
+          return_list <- lapply(filenames,function(f) { new_row(metadata,f) })
+          return(return_list)
+        })
+        
+        # before: List of lists of data.tables
+        chip_list <- unlist(chip_list[!is.na(chip_list)],recursive=FALSE)
+        # after:  List of data.tables
+        
+        chip_biosources <- unique(sapply(chip_list,function(x){ x$biosource }))
+        
+        # 3rd Step: Add ATAC experiments to csv if biosource has available ChiP data
+        
+        atac_list <- lapply(atac_metadata,function(m) {
+          if(m$sample_info$biosource_name %in% chip_biosources) {
+            return_list <- vector("list",length(chrs))
+            filenames <- chrom_in_filename(m$name,chrs)
+            return_list <- lapply(filenames,function(f) { new_row(m,f) })
+            return(return_list)
+          } else {
+            return(NA)
+          }
+        })
+        atac_list <- unlist(atac_list[!is.na(atac_list)],recursive=FALSE)
+        
+        csv_data[[which(genomes==genome)]] <- rbindlist(c(chip_list,atac_list),fill=TRUE)
+          
+      } else {
+        warning(paste(genome,"No CHIP-seq data available for given arguments",sep=": "))
       }
-      return(return_list)
     }
-  })
-  atac_list <- unlist(atac_list[!is.na(atac_list)],recursive = FALSE)
-  
-  csv_data <- rbindlist(c(chip_list,atac_list),fill=TRUE)
-  
+  }
   output_file <- paste(outfile,"csv",sep=".")
   
-  if(nrow(csv_data) > 0) {
+  if(is.null(csv_data)) {
+    stop("No data available for CSV")
+  } else {
+    csv_data <- unlist(csv_data,recursive=FALSE) # flatten genome lists into one list
     write.csv(csv_data,file=output_file,na="",row.names=FALSE)
     # remove newline characters
     t <- read.csv(file=output_file,na.strings = "\n")
@@ -142,4 +195,4 @@ create_linking_table <- function(genomes,chip_type,chip_marks,outfile) {
   }
 }
 
-create_linking_table(args$genomes,args$type,args$marks,args$output)
+create_linking_table(args$genomes,args$chromosomes,args$biosources,args$type,args$atactype,args$marks,args$output)
