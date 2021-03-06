e1d1900b288a794c042abb304da53387e8dfb567
kmlr81 <kristina.mueller02@gmail.com>
2021-03-06 15:24:15 +0100

    #9 .log file generated in log_normalize_file() is now bw format, if inital file is also bw format and methods get_min_max() and min_max_scale_file() have been adapted to handle .log file in bw format.

--- bin/scripts/normalize_signal_values.py
+++ bin/scripts/normalize_signal_values.py
@@ -66,57 +66,72 @@ def normalize_all(linkage_table_new_path, linkage_table_old_path=""):
     # Find global min and global max values
 
 
-def log_scale_file(file_path, column_names):
+def log_scale_file(file_path, column_names=None):
     """
     Method takes a file and writes a new file from it, which is a copy of the
-    old one but with log scaled signal values and the new file ending .log.
+    old one but with log scaled signal values in case the old file has bigWig
+    format or a one column file containing only log signal values in case the
+    old file has bed or bedGraph format. The new file is named after the
+    old one with an additional ending .log.
 
     :param file_path: String of path to file to be read in
-    :param column_names: List of Strings with column names in file
+    :param column_names: List of Strings with column names in file. Is set to
+           None, if not given
     :return: log_file_path: String containing path to log-scaled file
     """
     log_file_path = file_path + ".log"
-    is_big_wig = is_bigwig(file_path)
 
-    if is_big_wig:
+    if is_big_wig(file_path):
         bw = pyBigWig.open(file_path)
         chrs = bw.chroms()
-        log_values = []
+        header = list(chrs.items())
+        bw_log = pyBigWig.open(log_file_path, 'w')
+        bw_log.addHeader(header)
 
         for chrom in chrs.keys():
             intervals = bw.intervals(chrom)
+            chromosomes = [chrom] * len(intervals)
+            starts = [interval[0] for interval in intervals]
+            ends = [interval[1] for interval in intervals]
             signal_values = [interval[2] for interval in intervals]
-            log_vals = numpy.log(signal_values)
-            log_vals = [str(value) + "\n" for value in log_vals]
-            log_values.extend(log_vals)
+            log_values = numpy.log(signal_values)
+            bw_log.addEntries(chromosomes, starts, ends=ends, values=log_values)
 
         bw.close()
+        bw_log.close()
+
     else:
         idx = get_value_index(column_names)
         signal_values = numpy.loadtxt(file_path, usecols=[idx])
         log_values = numpy.log(signal_values)
         log_values = [str(value) + "\n" for value in log_values]
 
-    with open(log_file_path, 'w') as log_file:
-        log_file.writelines(log_values)
+        with open(log_file_path, 'w') as log_file:
+            log_file.writelines(log_values)
 
     return log_file_path
 
 
-def get_min_max(log_file_path, min_val, max_val):
+def get_min_max(log_file_path, min_val=math.inf, max_val=-math.inf):
     """
     Method finds min value and max value in a .log file.
 
     :param log_file_path: String with path to file with log values
-    :param min_val: Start value for min. If method is called for first time
-                math.Inf is recommended
-    :param max_val: Start value for max. If method is called for the first time
-                -math.Inf is recommended
+    :param min_val: Start value for min. Is set to math.Inf if not given
+    :param max_val: Start value for max. Is set to -math.Inf if not given
     :return: min and max as float
     """
-    signal_values = numpy.loadtxt(log_file_path, usecols=[0])
-    tmp_min = min(signal_values)
-    tmp_max = max(signal_values)
+    if is_big_wig(log_file_path):
+        log_bw = pyBigWig.open(log_file_path)
+        header = log_bw.header()
+        tmp_min = header['minVal']
+        tmp_max = header['maxVal']
+        log_bw.close()
+
+    else:
+        signal_values = numpy.loadtxt(log_file_path, usecols=[0])
+        tmp_min = min(signal_values)
+        tmp_max = max(signal_values)
 
     min_val = tmp_min if tmp_min < min_val else min_val
     max_val = tmp_max if tmp_max > max_val else max_val
@@ -124,41 +139,48 @@ def get_min_max(log_file_path, min_val, max_val):
     return min_val, max_val
 
 
-def min_max_scale_file(file_path, log_file_path, column_names, min_val,
-                       max_val):
+def min_max_scale_file(file_path, log_file_path, min_val,
+                       max_val, column_names=None):
     """
     Method min-max scales values in file to a range between 0 and 1.
 
     :param file_path: String with path to file to be scaled
     :param log_file_path: String with path to file with log-scaled values of
-    original file
-    :param column_names: List of strings with names of columns in file
+           original file
     :param max_val: Global max value
     :param min_val: Global min value
+    :param column_names: List of strings with names of columns in file,
+           only needs to be given, if file is not bigWig format
     """
     tmp_file_path = file_path + ".tmp"
-    log_values = numpy.loadtxt(log_file_path, usecols=[0])
-    min_max_values = [(x - min_val) / (max_val - min_val) for x in log_values]
-    is_big_wig = is_bigwig(file_path)
 
-    if is_big_wig:
+    if is_big_wig(file_path) and is_big_wig(log_file_path):
         bw = pyBigWig.open(file_path)
+        log_bw = pyBigWig.open(log_file_path)
         chrs = bw.chroms()
         header = list(chrs.items())
-        bw_new = pyBigWig.open(tmp_file_path, 'w')
-        bw_new.addHeader(header)
+        min_max_bw = pyBigWig.open(tmp_file_path, 'w')
+        min_max_bw.addHeader(header)
 
         for chrom in chrs.keys():
             intervals = bw.intervals(chrom)
+            log_intervals = log_bw.intervals(chrom)
             chromosomes = [chrom] * len(intervals)
             starts = [interval[0] for interval in intervals]
             ends = [interval[1] for interval in intervals]
-            bw_new.addEntries(chromosomes, starts, ends=ends,
+            min_max_values = [(interval[2] - min_val) / (max_val - min_val) for
+                              interval in log_intervals]
+            min_max_bw.addEntries(chromosomes, starts, ends=ends,
                               values=min_max_values)
 
         bw.close()
-        bw_new.close()
+        log_bw.close()
+        min_max_bw.close()
+
     else:
+        log_values = numpy.loadtxt(log_file_path, usecols=[0])
+        min_max_values = [(x - min_val) / (max_val - min_val) for x in
+                          log_values]
         idx = get_value_index(column_names)
         cnt = 0
 
@@ -176,11 +198,11 @@ def min_max_scale_file(file_path, log_file_path, column_names, min_val,
 
 def get_value_index(column_names):
     """
-    Method gets column index of signal value in file.
+    Method gets column index of signal value in bed or bedGraph file.
 
     :param column_names: List of strings with names of columns in file
     :return: index: Integer representing index of column with signal value in
-                    file
+             file
     """
     if "SIGNAL_VALUE" in column_names:
         index = column_names.index("SIGNAL_VALUE")
@@ -190,18 +212,18 @@ def get_value_index(column_names):
     return index
 
 
-def is_bigwig(file_path):
+def is_big_wig(file_path):
     """
     Method checks if a file has bigWig format or not.
 
     :param file_path: String representing path to file
-    :return: Boolean value, True if file has bigWig format, False if not
+    :return: Boolean value. True if file has bigWig format, False if not
     """
     try:
         bw = pyBigWig.open(file_path)
-        is_big_wig = bw.isBigWig()
+        is_bw = bw.isBigWig()
         bw.close()
     except:
-        is_big_wig = False
+        is_bw = False
 
-    return is_big_wig
+    return is_bw
