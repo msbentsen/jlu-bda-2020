f4599d0047e0d98434e76b5ecfd7d6e062cba7e8
nknoppik <74065825+nknoppik@users.noreply.github.com>
2021-03-10 14:14:56 +0100

    Added functions for getting raw data

--- bin/scripts/visualization_app_api_start.py
+++ bin/scripts/visualization_app_api_start.py
@@ -6,7 +6,7 @@ Created on Wed Feb 17 16:32:30 2021
 """
 
 # import main Flask class and request object
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 import api_calls
 
@@ -17,20 +17,25 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/getTreeData', methods=["GET"])
 def getTreeData():
-    result_dict_cp = {"data":[{'item': 'GM12878', 'type': '', 'belongsTo': '', 'checked': False, 'children': [{'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}]}]}
-    #return api_calls.get_biosource_list_for_tree()
-    return result_dict_cp
+    #result_dict_cp = {"data":[{'item': 'GM12878', 'type': '', 'belongsTo': '', 'checked': False, 'children': [{'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}, {'item': 'ATF2_ENCFF210HTZ', 'type': 'tf', 'belongsTo': 'GM12878', 'checked': False, 'children': []}]}]}
+    return api_calls.get_biosource_list_for_tree()
+    #return result_dict_cp
 
-@app.route('/getGraphPaths', methods=["POST"])
-def getGraphPaths():
+# =============================================================================
+# @app.route('/getGraphPaths', methods=["POST"])
+# def getGraphPaths():
+#     request_data = request.get_json()
+#     #data = {"data": request_data, "hey": "there"}
+#     #checked_dict = api_calls.getChecked(request_data)
+#     
+#     return api_calls.getGraphs(api_calls.getChecked(request_data))
+# =============================================================================
+
+@app.route('/getRawData', methods=["POST"])
+def getRawData():
     request_data = request.get_json()
-    #data = {"data": request_data, "hey": "there"}
-    
-    return api_calls.getPathList(request_data)
+    return api_calls.getRawData(api_calls.getChecked(request_data))
 
-@app.route('/json-example')
-def json_example():
-    return 'JSON Object Example'
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
